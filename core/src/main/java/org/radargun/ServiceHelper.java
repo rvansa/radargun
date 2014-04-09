381b32f12a2fb2bcab0733cafa772d84faca00f1
rvansa <rvansa@redhat.com>
2014-04-09 13:34:43 +0200

    Setup properties are evaluated on each node individually * added slaveIndex as built-in setup property * renamed the built-in properties to shorter form

--- core/src/main/java/org/radargun/ServiceHelper.java
+++ core/src/main/java/org/radargun/ServiceHelper.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.radargun.config.Evaluator;
 import org.radargun.config.InitHelper;
 import org.radargun.config.PropertyHelper;
 import org.radargun.utils.Utils;
@@ -12,7 +13,9 @@ import org.radargun.utils.Utils;
  * @author Radim Vansa &lt;rvansa@redhat.com&gt;
  */
 public class ServiceHelper {
-   public static Object createService(ClassLoader classLoader, String plugin, String service, String configName, String configFile, Map<String, String> properties) {
+   public static Object createService(ClassLoader classLoader, String plugin, String service, String configName,
+                                      String configFile, int slaveIndex,
+                                      Map<String, String> properties, Map<String, String> extras) {
 
       Thread.currentThread().setContextClassLoader(classLoader);
 
@@ -40,11 +43,24 @@ public class ServiceHelper {
          throw new RuntimeException("Cannot instantiate service " + serviceClassName, e);
       }
 
-      Map<String, String> configProperties = new HashMap<String, String>(properties);
-      configProperties.put(Service.PROP_CONFIG_NAME, configName);
-      configProperties.put(Service.PROP_PLUGIN, plugin);
+      // The properties are evaluated only on the slave, using the extras (such as ${slave.index} etc...)
+      Map<String, String> configProperties = new HashMap<String, String>();
+      Map<String, String> backupExtras = new HashMap<String, String>();
+      for (Map.Entry<String, String> extra : extras.entrySet()) {
+         backupExtras.put(extra.getKey(), System.getProperty(extra.getKey()));
+         System.setProperty(extra.getKey(), extra.getValue());
+      }
+      for (Map.Entry<String, String> entry : properties.entrySet()) {
+         configProperties.put(entry.getKey(), Evaluator.parseString(entry.getValue()));
+      }
+      for (Map.Entry<String, String> backup : backupExtras.entrySet()) {
+         System.setProperty(backup.getKey(), backup.getValue() == null ? "" : backup.getValue());
+      }
+      configProperties.put(Service.SLAVE_INDEX, String.valueOf(slaveIndex));
+      configProperties.put(Service.CONFIG_NAME, configName);
+      configProperties.put(Service.PLUGIN, plugin);
       if (configFile != null) {
-         configProperties.put(Service.PROP_FILE, configFile);
+         configProperties.put(Service.FILE, configFile);
       }
       PropertyHelper.setProperties(instance, configProperties, true, true);
       InitHelper.init(instance);
