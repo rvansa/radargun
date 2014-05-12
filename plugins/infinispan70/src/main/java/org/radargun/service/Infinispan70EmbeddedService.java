ede726c12b95049c7cb006a8fedebb8773a2b7a9
rvansa <rvansa@redhat.com>
2014-05-12 15:53:11 +0200

    Adapting to Infinispan 7.0 API changes

---
+++ plugins/infinispan70/src/main/java/org/radargun/service/Infinispan70EmbeddedService.java
@@ -0,0 +1,30 @@
+package org.radargun.service;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.infinispan.commons.util.FileLookup;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
+import org.radargun.Service;
+import org.radargun.traits.CacheListeners;
+import org.radargun.traits.ProvidesTrait;
+
+/**
+ * @author Radim Vansa &lt;rvansa@redhat.com&gt;
+ */
+@Service(doc = InfinispanEmbeddedService.SERVICE_DESCRIPTION)
+public class Infinispan70EmbeddedService extends Infinispan60EmbeddedService {
+
+   @Override
+   protected ConfigurationBuilderHolder createConfiguration(String configFile) throws FileNotFoundException {
+      ClassLoader classLoader = getClass().getClassLoader();
+      InputStream input = new FileLookup().lookupFileStrict(configFile, classLoader);
+      return new ParserRegistry(classLoader).parse(input);
+   }
+
+   @ProvidesTrait
+   public CacheListeners createListeners() {
+      return new InfinispanCacheListeners(this);
+   }
+}
